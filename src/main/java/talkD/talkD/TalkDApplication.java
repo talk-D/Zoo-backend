2ac42188612d512c0ccbf54b4863d5da391d4a9e
sulsikan <oju0916@naver.com>
2024-03-29 16:32:30 +0900

    talkD

---
+++ src/main/java/talkD/talkD/TalkDApplication.java
@@ -0,0 +1,13 @@
+package talkD.talkD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talkD/talkD/TalkDApplicationTests.java
@@ -0,0 +1,13 @@
+package talkD.talkD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
