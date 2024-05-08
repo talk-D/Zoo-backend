40621bbfb4c93f52a5662caa4dcbe0f01a6f3b6b
sulsikan <oju0916@naver.com>
2024-05-08 22:03:27 +0900

    잔디테스트

--- src/main/java/talkD/talkD/controller/HomeController.java
+++ src/main/java/talkD/talkD/controller/HomeController.java
@@ -3,7 +3,7 @@ package talkD.talkD.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+//git 잔디 테스트 2
 @Controller
 public class HomeController {
 
